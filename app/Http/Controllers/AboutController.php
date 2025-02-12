17e69394791bff3b2d70fb55a4ddb59dec7a3547
Gaurav Chaulagain <talktogaurav07@gmail.com>
2025-02-12 20:56:07 +0545

    commit

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -17,8 +17,8 @@ class AboutController extends Controller
             'title' => 'required|string|max:255',
             'content' => 'required|string',
             'media_type' => 'required|in:images,videos',
-            'images.*' => 'nullable|image|max:1024', // Each image has a 1MB limit
-            'videos' => 'nullable|mimes:mp4,avi,webm|max:20480', // Video has a 20MB limit
+            'images.*' => 'nullable|image|max:1024',
+            'videos' => 'nullable|mimes:mp4,avi,webm|max:20480',
         ]);
 
         $validated[($request->media_type === 'images' && $request->hasFile('images')) ? 'images' : 'video'] =
@@ -26,8 +26,7 @@ class AboutController extends Controller
             ? array_map(fn($image) => $image->store('images', 'public'), $request->file('images'))
             : ($request->media_type === 'videos' ? $request->file('videos')->store('videos', 'public') : null);
 
-        // laravel is crazyyy :D
 
-        // return redirect()->route('about')->with('success', 'Data submitted successfully!');
+        return redirect()->route('about')->with('success', 'Data submitted successfully!');
     }
 }
