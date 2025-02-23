d2a0b0b59b0dcd8664129c90f35dc09acb40bf6b
Gaurav Chaulagain <talktogaurav07@gmail.com>
2025-02-23 09:55:50 +0545

    update bird-form

--- resources/views/livewire/bird-form.blade.php
+++ resources/views/livewire/bird-form.blade.php
@@ -8,6 +8,8 @@
             <x-input-label for="notes" :value="('Notes')" />
             <textarea wire:model="notes" class="mt-1 block mb-4"></textarea>
         </div>
+        <x-primary-button wire:click="delete"
+            wire:confirm="Are you sure you want to delete this?">Delete</x-primary-button>
         <x-primary-button>Add Bird Count</x-primary-button>
     </form>
 </div>
