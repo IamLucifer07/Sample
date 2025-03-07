e0ffdd3e4fed6ef133aeecbca41b81a09e180190
Gaurav Chaulagain <talktogaurav07@gmail.com>
2025-03-07 14:55:38 +0545

    Add language resources

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'Select Media Type' => 'Select Media Type',
+    'Images' => 'Images',
+    'Videos' => 'Videos',
+    'Submit' => 'Submit',
+    'Dashboard' => 'Dashboard',
+    'Admin Panel' => 'Admin Panel',
+    'Login' => 'Login',
+    'About' => 'About',
+    'Contact' => 'Contact',
+    'Gallery' => 'Gallery',
+    'Testimonials' => 'Testimonials',
+];
