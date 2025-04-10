78aac5078f15ad780abed5f4bfe8c0d0bbfc5765
Gaurav Chaulagain <talktogaurav07@gmail.com>
2025-04-10 16:17:16 +0545

    api

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\DataController;
 
 
-Route::namespace('Api')->group(function () {
-    Route::get('/datas', [DataController::class, 'index']);
-    Route::post('/datas', [DataController::class, 'store']);
-    Route::get('/datas/{id}', [DataController::class, 'show']);
-    Route::put('/datas/{id}', [DataController::class, 'update']);
-    Route::delete('/datas/{id}', [DataController::class, 'destroy']);
-});
+
+Route::get('/datas', [DataController::class, 'index']);
+Route::post('/datas', [DataController::class, 'store']);
+Route::get('/datas/{id}', [DataController::class, 'show']);
+Route::put('/datas/{id}', [DataController::class, 'update']);
+Route::delete('/datas/{id}', [DataController::class, 'destroy']);
