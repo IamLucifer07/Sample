821d375e4e9c46118442a075bc213654de41421d
Gaurav Chaulagain <talktogaurav07@gmail.com>
2025-06-09 11:48:55 +0545

    Bird Form

--- resources/views/livewire/bird-form.blade.php
+++ resources/views/livewire/bird-form.blade.php
@@ -5,7 +5,7 @@
             <x-text-input wire:model="count" type="number" />
         </div>
         <div>
-            <x-input-label for="notes" :value="('Notes')" />
+            {{-- <x-input-label for="notes" :value="('Notes')" /> --}}
             <textarea wire:model="notes" class="mt-1 block mb-4"></textarea>
         </div>
         {{-- <x-primary-button wire:click="delete"
